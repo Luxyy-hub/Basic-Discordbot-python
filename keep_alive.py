211b8c689ffd709fab356479c241802ef4a88d5a
Project-Luxy <96788441+Project-Luxy@users.noreply.github.com>
2022-01-21 13:01:32 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from threading import Thread
 app = Flask('app')
 
 @app.route('/')
@@ -6,3 +7,7 @@ def hello_world():
   return 'Hi!'
 
 app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
